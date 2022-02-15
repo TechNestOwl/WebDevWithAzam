568769762752a3f837684cb5c3aa14b7dd72b295
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2022-02-15 10:37:29 -0500

    increment counter

--- React/hello_react/src/App.js
+++ React/hello_react/src/App.js
@@ -1,18 +1,63 @@
-import { Component } from 'react';
-import Hello from './Hello';
+import React, { Component } from 'react'
 
 
-//app class inherits from Component class
 class App extends Component {
-    render(){
-      return(
+
+    constructor() {
+      // call the constructor of the parent class 
+      // We call super to make sure that component is initialized 
+      super() 
+
+      // create a state/ private state/ local state 
+      // this state is not visible to accessible from outside 
+      // this component. 
+      this.state = {
+        counter: 0, 
+        username: 'John Doe'
+      }
+
+    }
+
+    // inside arrow functions the value of this is not undefined 
+    handleIncrement = () => {
+      // state can NOT be changed 
+      //this.state.counter += 1
+      // state can be REPLACED 
+      // When you set the state render is called automatically
+      this.setState({
+        counter: this.state.counter + 1, 
+        username: 'Mary Doe'
+        // since setState updates partial state 
+        // if you don't change username then you don't need to add it to the new state. It will automatically be added 
+        // username: this.state.username 
+      })
+    }
+
+    /*
+    handleIncrement() {
+      console.log(this)
+      this.counter += 1 
+      console.log(this.counter)
+      console.log('handle increment')
+    } */
+
+    render() {
+
+      // NOT A GOOD IDEA 
+      // This will cause infinite loop 
+      /*
+      this.setState({
+        counter: this.state.counter + 1 
+      }) */
+
+
+      return (
         <div>
-          <h1>Greetings</h1>
-          <Hello firstName="John" lastName="Doe" />
+          <h1>{this.state.counter}</h1>
+          <button onClick = {this.handleIncrement}>Increment</button>
         </div>
       )
     }
 }
 
-//This is so other components can import App Component
-export default App;
+export default App 
---
+++ React/hello_react/src/Friends.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+
+//taskList component is responsible for displaying list of tasks
+class Friends extends Component {
+    render(){
+        // const friendsList = {this.props.allFriends}
+        
+        return (
+
+            <div>
+                <h1>Friends List</h1>
+
+            </div>
+        )
+    }
+};
+
+export default Friends;
